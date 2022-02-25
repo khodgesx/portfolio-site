c073d4a881e2bee05924444d5c595014aded6c7a
khodges <zkellyhodges@gmail.com>
2022-02-25 11:32:39 -0800

    re-styled the info sections on my homepage - browser

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+
+//hello button from pill shape to circle and photo reveal
 $("#hello").on("click", function(){
     $(".pill").toggleClass("to-circle");
 });
 
+//click on #hamburger image and add class menu open to menu
 $('#hamburger').on('click', ()=>{
     $('.menu').addClass('menu-open')
 });
+//click X and remove class from menu/close it
 $('#menu-close').on('click', ()=>{
     $('.menu').removeClass('menu-open')
 })
