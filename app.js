47cf4524a951059e5127b06b5bbadfbd9d1f79fa
khodges <zkellyhodges@gmail.com>
2022-02-25 10:54:12 -0800

    made the menu hamburger for mobile

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 $("#hello").on("click", function(){
     $(".pill").toggleClass("to-circle");
 });
+
+$('#hamburger').on('click', ()=>{
+    $('.menu').addClass('menu-open')
+});
+$('#menu-close').on('click', ()=>{
+    $('.menu').removeClass('menu-open')
+})
